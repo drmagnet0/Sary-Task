62320f2aa2da2c19152247611c98a0bc27607cfe
drmagnet0 <abdelrahman.ui@gmail.com>
2021-08-29 15:21:39 +0200

    start working on table

--- src/app/modules/heros/components/heros-list/heros-list.component.ts
+++ src/app/modules/heros/components/heros-list/heros-list.component.ts
@@ -1,5 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { Hero } from '../../models/hero';
 import { HerosService } from '../../services/heros.service';
+
+export type SortColumn = keyof Hero | string | '';
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: { [key: string]: SortDirection } = {
+  asc: 'desc',
+  desc: '',
+  '': 'asc',
+};
+
+const compare = (v1: string | number, v2: string | number) =>
+  v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: SortColumn;
+  direction: SortDirection;
+}
+
+@Component({
+  template: '',
+})
+export class NgbdSortableHeader {
+  @Input() sortable = '';
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({ column: this.sortable, direction: this.direction });
+  }
+}
 
 @Component({
   selector: 'app-heros-list',
@@ -8,8 +47,10 @@ import { HerosService } from '../../services/heros.service';
 })
 export class HerosListComponent implements OnInit {
   heros;
+  herosFilters;
   @Input() set filters(value) {
     if (value) {
+      this.herosFilters = value;
       console.log(value);
     }
   }
@@ -25,7 +66,26 @@ export class HerosListComponent implements OnInit {
     console.log(this.heros);
   }
 
-  onSort(e) {
-    console.log(e);
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  onSort({ column, direction }: SortEvent) {
+    console.log(column, direction);
+
+    // resetting other headers
+    this.headers.forEach((header) => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    // sorting countries
+    if (direction === '' || column === '') {
+      this.heros = this.heros;
+    } else {
+      this.heros = [...this.heros].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
   }
 }
--- src/app/modules/heros/heros.module.ts
+++ src/app/modules/heros/heros.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HerosListComponent } from './components/heros-list/heros-list.component';
+import {
+  HerosListComponent,
+  NgbdSortableHeader,
+} from './components/heros-list/heros-list.component';
 import { HerosFiltersComponent } from './components/heros-filters/heros-filters.component';
-import { HeroComponent } from './models/hero/hero.component';
 import { HerosRoutingModule } from './heros-routing.module';
 import { HerosComponent } from './pages/heros/heros.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Hero } from './models/hero';
 
 @NgModule({
   declarations: [
     HerosListComponent,
     HerosFiltersComponent,
-    HeroComponent,
     HerosComponent,
+    NgbdSortableHeader,
   ],
   imports: [
     CommonModule,
